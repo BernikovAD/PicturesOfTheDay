10390d6ab7c9470df19180531f2ba188b58d8aa8
berni <bernikov_1993@mail.ru>
2021-09-21 19:52:35 +0500

    1. Костомизировал прокрутку ViewPager2

--- app/src/main/java/com/example/picturesoftheday/view/planets/FragmentPlanets.kt
+++ app/src/main/java/com/example/picturesoftheday/view/planets/FragmentPlanets.kt
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment
 import com.example.picturesoftheday.databinding.FragmentPlanetsBinding
 import com.example.picturesoftheday.view.settings.ViewPagerAdapter
 import com.google.android.material.tabs.TabLayoutMediator
+import kotlin.math.abs
+import kotlin.math.max
 
 
 class FragmentPlanets : Fragment() {
@@ -26,12 +28,42 @@ class FragmentPlanets : Fragment() {
 
     }
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentPlanetsBinding.inflate(inflater, container, false)
         binding.viewPager.adapter = ViewPagerAdapter(this)
+        binding.viewPager.setPageTransformer { _, position ->
+            view?.apply {
+                val pageWidth = width
+                val pageHeight = height
+                when {
+                    position < -1 -> {
+                        alpha = 0f
+                    }
+                    position <= 1 -> {
+                        val scaleFactor = max(MIN_SCALE, 1 - abs(position))
+                        val vertMargin = pageHeight * (1 - scaleFactor) / 2
+                        val horzMargin = pageWidth * (1 - scaleFactor) / 2
+                        translationX = if (position < 0) {
+                            horzMargin - vertMargin / 2
+                        } else {
+                            horzMargin + vertMargin / 2
+                        }
+                        scaleX = scaleFactor
+                        scaleY = scaleFactor
+                        alpha = (MIN_ALPHA +
+                                (((scaleFactor - MIN_SCALE) / (1 - MIN_SCALE)) * (1 - MIN_ALPHA)))
+                    }
+                    else -> {
+                        alpha = 0f
+                    }
+                }
+            }
+
+        }
         TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             when (position) {
                 0 -> tab.text = "Earth"
@@ -44,8 +76,11 @@ class FragmentPlanets : Fragment() {
     }
 
     companion object {
-
+        private const val MIN_SCALE = 0.85f
+        private const val MIN_ALPHA = 0.5f
         fun newInstance() = FragmentPlanets()
 
     }
+
 }
+
